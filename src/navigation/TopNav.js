37d3028c9645328b954644424130cc4a5c880260
Jinyuan Chen <picowithleo@gmail.com>
2020-08-17 21:47:33 +1000

    add navigation bar

---
+++ src/navigation/TopNav.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Link } from 'react-router-dom';
+
+// import './topNav.scss';
+
+const NavContainer = styled.ul`
+    display: flex;
+    list-style: none;
+    color: ${props => props.isRed && 'red'};
+
+    a {
+        text-decoration: none;
+    }
+
+
+`;
+
+const TopNav = () => {
+
+    
+
+
+    return (
+        <nav>
+            {/* <ul className="nav"> */}
+            <NavContainer isRed>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+            </NavContainer>
+            {/* </ul> */}
+        </nav>
+        
+    );
+};
+
+export default TopNav;
