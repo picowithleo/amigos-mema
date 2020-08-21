b3e6f3ec471d3bb39e95772d0bdbbf7419c94135
Xiwen Li <Evelyn@MX7.gateway>
2020-08-21 17:46:46 +1000

    add learn.js page

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,12 +4,14 @@ import Home from '../page/Home';
 import Profile from '../page/Profile';
 import Login from '../auth/Login';
 import Register from '../auth/Register';
+import Learn from '../page/Learn';
 
 const Routes = () => {
   return (
     <Switch>
         <Redirect exact from="/" to="/login" />
         <Route exact path="/home" component={Home} />
+        <Route exact path="/learn" component={Learn} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
