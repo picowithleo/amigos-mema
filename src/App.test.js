2828247210bb4907edd9009a53e27730ff1f2f3c
Jinyuan Chen <picowithleo@gmail.com>
2020-09-21 09:26:15 +1000

    Add StaticRouter in test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { StaticRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <StaticRouter>
+      <App />
+    </StaticRouter>
+  );
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
