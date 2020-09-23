63e6ff37409e63b33f13f3fa5ef95fa8bf9daf21
sobermx7 <evelynlxw@gmail.com>
2020-09-23 19:39:39 +1000

    fix home page

--- src/page/Home.js
+++ src/page/Home.js
@@ -9,9 +9,9 @@ import Typography from '@material-ui/core/Typography';
 import MicIcon from '@material-ui/icons/Mic';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../page/Home.css';
 import picture1 from '../assets/img/learning.jpg';
 import picture2 from '../assets/img/review.jpg';
+import '../page/Home.css';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -19,7 +19,6 @@ const useStyles = makeStyles((theme) => ({
     learnbackground: {
         marginTop: "24px",
         width: '100%',
-
         position: 'relative',
         height: '100px',
         backgroundColor: '#9CCC9D',
@@ -31,16 +30,12 @@ const useStyles = makeStyles((theme) => ({
         width: '80%',
         height: '90px',
         backgroundColor: '#FFFFFF',
-
     },
     title: {
-
         textAlign: 'left',
         color: '#A9A9A9',
-
     },
     wenzi: {
-
         textAlign: 'center',
         position: 'relative',
         fontSize: "40px",
@@ -85,7 +80,6 @@ const useStyles = makeStyles((theme) => ({
     micbutton: {
         position: 'relative',
         marginTop: "-15px",
-
     },
 
 
@@ -112,8 +106,8 @@ export default function CustomizedInputBase() {
                     <h><b>talk to MEMA</b></h>
                 </Box>
                 <Typography>
-                    {/* <LockOutlinedIcon /> */}
-                    <img className="mema" alt="Mema" component={Link} to="/Mema" />
+                    <img className="mema" component={Link} to="/Mema" />
+                    {/* <img className="mema" alt="Mema" component={Link} to="/Mema" /> */}
                 </Typography>
                 <IconButton type="speech" className={classes.micbutton} aria-label="speech" >
                     <BottomNavigationAction label="Discover" value="Discover" icon={<MicIcon />} component={Link} to="/Mema" />
@@ -122,10 +116,9 @@ export default function CustomizedInputBase() {
             <List className={classes.content2}>
                 <Box className={classes.title}>
                     <h><b>vocabulary book</b></h>
-
                 </Box>
                 <Button variant="outlined" className={classes.button1} component={Link} to="/learning"></Button>
-                <Button variant="outlined" className={classes.button2} component={Link} to="/review">s</Button>
+                <Button variant="outlined" className={classes.button2} component={Link} to="/review"></Button>
             </List>
         </Box>
 
