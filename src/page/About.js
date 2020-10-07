bf63184b4385e75b8ba3b90e2aac307f46518138
sobermx7 <evelynlxw@gmail.com>
2020-10-07 18:17:34 +1000

    add about info

--- src/page/About.js
+++ src/page/About.js
@@ -17,9 +17,15 @@ const useStyles = makeStyles((theme) => ({
     },
     appbar: {
         position: 'relative',
-        backgroundColor:'#8bc38c',
+        backgroundColor: '#8bc38c',
         marginTop: '-8px',
     },
+    about: {
+        margin: '5%',
+        backgroundColor: '#ffffff',
+        textAlign: 'left',
+        fontSize: '1.2rem'
+    }
 }));
 
 export default function CustomizedInputBase() {
@@ -40,6 +46,19 @@ export default function CustomizedInputBase() {
                     </Toolbar>
                 </AppBar>
             </Box>
+            <Typography className={classes.about}>
+                <div style={{ margin: '5%' }}>
+                    MEMA app is a language education web application specially designed for the elderly.
+                    <br></br>
+                    <br></br>
+                    According to the research on the relationship between cognitive decline and age,
+                    it was seen that the most effective age to start these cognitive interventions would be when people are approximately 50.
+                    <br></br>
+                    <br></br>
+                    In order to optimally combat this decline, stakeholders for our impending solution would have a heavy emphasis on this age bracket.
+                    Thus, the main target users are people around 50 years old.
+                </div>
+            </Typography>
         </List>
     );
 }
