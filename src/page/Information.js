03d793620f4330fce8d0fa1cb7695040008b0c3e
kewen <s4543383@student.uq.edu.au>
2020-10-19 15:43:44 +1000

    111

--- src/page/Information.js
+++ src/page/Information.js
@@ -1,11 +1,6 @@
 import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import { makeStyles } from '@material-ui/core/styles';
@@ -14,14 +9,24 @@ import Typography from '@material-ui/core/Typography';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import information from '../api/information';
+import new1 from '../assets/img/new1.png';
+import new2 from '../assets/img/new2.png';
+import new3 from '../assets/img/new3.png';
+import ListItem from '@material-ui/core/ListItem';
+import '../page/Information.css';
 
 const useStyles = makeStyles((theme) => ({
 
   root1: {
     padding: "20px",
-
   },
 
+  root: {
+    border: "2px solid grey",
+    borderRadius: '10px',
+    padding: '5px'
+  },
   appbar: {
     position: 'relative',
     backgroundColor:'#8bc38c',
@@ -33,10 +38,20 @@ const useStyles = makeStyles((theme) => ({
     padding: "20px",
     fontSize: "20px",
     color: 'grey',
-  }
+  },
+  langbox: {
+    width: '90%',
+    marginLeft: '5%',
+    display: 'flex',
+    height: '200px',
+    flexDirection: 'column',
+    // backgroundColor: theme.palette.background.paper,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  },
 
 }));
-
 
 export default function Information() {
   const classes = useStyles();
@@ -55,57 +70,40 @@ export default function Information() {
         </Toolbar>
       </AppBar>
 
-      <Card className={classes.root}>
-        <CardActionArea className={classes.title}>
-          <CardMedia
-            component="img"
-            alt="Title"
-            height="140"
-            image=""
-            title="XXXXXXXXX"
-          />
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-              XXXXXXXXXXXXXXXXXX
-          </Typography>
-          </CardContent>
+      <Card className={classes.root1}>
+        <a href={information[0].link} className="a">
+        <CardActionArea className={classes.root}>
+          <Typography style={{ fontSize: '1.1rem', fontWeight: 'bold', textAlign: 'justify'}}>
+              {information[0].title}
+             </Typography>
+             <ListItem className={classes.langbox} style={{ backgroundImage: 'url(' + new1 + ')' }}>
+          </ListItem>
         </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-        </Button>
-          <Button size="small" color="primary">
-            Learn More
-        </Button>
-        </CardActions>
+        </a>
       </Card>
 
-      <Box className={classes.root1}></Box>
-
+      <Card className={classes.root1}>
+        <a href={information[1].link} className="a">
+        <CardActionArea className={classes.root}>
+          <Typography style={{ fontSize: '1.1rem', fontWeight: 'bold', textAlign: 'justify'}}>
+              {information[1].title}
+             </Typography>
+             <ListItem className={classes.langbox} style={{ backgroundImage: 'url(' + new2 + ')' }}>
+          </ListItem>
+        </CardActionArea>
+        </a>
+      </Card>
 
       <Card className={classes.root1}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            alt="XXXXXXXXX"
-            height="140"
-            image=""
-            title="XXXXXXXXX"
-          />
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-              XXXXXXXXXXXXXXXXXX
-          </Typography>
-          </CardContent>
+        <a href={information[2].link} className="a">
+        <CardActionArea className={classes.root}>
+          <Typography style={{ fontSize: '1.1rem', fontWeight: 'bold', textAlign: 'justify'}}>
+              {information[2].title}
+             </Typography>
+             <ListItem className={classes.langbox} style={{ backgroundImage: 'url(' + new3 + ')' }}>
+          </ListItem>
         </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-        </Button>
-          <Button size="small" color="primary">
-            Learn More
-        </Button>
-        </CardActions>
+        </a>
       </Card>
 
     </List>
